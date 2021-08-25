be273298252361afbfc81a62fafa90cb5a591094
MINTU325 <sainimintu34@gmail.com>
2021-08-25 09:30:04 +0530

    Buttom Navigation Bar

---
+++ app/src/main/java/com/example/instagram/HomeFragment/PostViewHolder.kt
@@ -0,0 +1,8 @@
+package com.example.instagram.HomeFragment
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class PostViewHolder(val view :View):RecyclerView.ViewHolder(view) {
+
+}
