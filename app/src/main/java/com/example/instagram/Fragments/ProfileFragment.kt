ab6f3c668a04b76f620493baefd96c98de8bb218
MINTU325 <sainimintu34@gmail.com>
2021-08-27 11:59:00 +0530

    pull from main

--- app/src/main/java/com/example/instagram/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/instagram/Fragments/ProfileFragment.kt
@@ -1,14 +1,20 @@
 package com.example.instagram.Fragments
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.instagram.R
+import com.theartofdev.edmodo.cropper.CropImage
+import kotlinx.android.synthetic.main.activity_add_post.*
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
-
+    private var ImageUri2: Uri? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,5 +23,22 @@ class ProfileFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        myprofile_image.setOnClickListener {
+            val intent = CropImage.activity(ImageUri2)
+                .getIntent(requireContext())
+            startActivityForResult(intent, CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE)
+        }
+    }
 
-}
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            val result2 = CropImage.getActivityResult(data)
+            ImageUri2 = result2.uri
+            myprofile_image.setImageURI(ImageUri2)
+        }
+    }}
+
